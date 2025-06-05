614ba4e8447a14a22c68911609625bd55290d538
borilio <borilio13@gmail.com>
2025-06-05 13:13:31 +0200

    Terminado el componente listado-tarjetas

--- src/app/components/listado-tarjetas/listado-tarjetas.ts
+++ src/app/components/listado-tarjetas/listado-tarjetas.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroeService } from '../../services/heroe.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-listado-tarjetas',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './listado-tarjetas.html',
   styleUrl: './listado-tarjetas.css'
 })
-export class ListadoTarjetas {
+export class ListadoTarjetas implements OnInit {
+  public heroes: Heroe[];
+
+  constructor(public heroeService: HeroeService) {
+    this.heroes = [];
+  }
+
+  ngOnInit(): void {
+    // La llamada al servicio debería hacerse siempre desde el método ngOnInit, para no hacerlo desde el constructor.
+    this.heroes = this.heroeService.getHeroes();
+  }
 
 }
